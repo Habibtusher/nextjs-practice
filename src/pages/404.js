5bc07cab6f30eb2e2a555c374a1615665ea3d8c7
Habibtusher <tusherhrt@gmail.com>
2023-07-24 09:18:58 +0600

    update

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <img width="100%" src='https://assets.website-files.com/600c68cb72173ece9c8958ed/6092052bc93b16ed467a22da_Frame%2085.png'/>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/pages/article/blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticleBlog = () => {
+    return (
+        <div>
+            <h1>This is article Blog</h1>
+        </div>
+    );
+};
+
+export default ArticleBlog;
